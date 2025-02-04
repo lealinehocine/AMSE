dca8cdc7b5860b1992cea47972024f49441fbeaa
lealine <lea.line.saad@gmail.com>
2025-02-04 17:53:37 +0100

    reprendre à embellir l'application : ajouter une carte

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -39,12 +39,13 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
           Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
+          BigCard(pair: pair),
 
           ElevatedButton(
             onPressed: () {
@@ -55,6 +56,23 @@ class MyHomePage extends StatelessWidget {
 
         ],
       ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Text(pair.asLowerCase),
     );
   }
 }
